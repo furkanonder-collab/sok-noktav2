f2d07037625fa81ad9682e5ff7688f856641e47f
furkanonder-collab <furkanonder@gmail.com>
2026-07-08 09:24:03 +0300

    Cache surumu v3 (yeni surum icin taze yukleme)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v2';
+var CACHE = 'sok-nokta-v3';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
