34d02b14d78044891b608aef650cb419403dd2d5
furkanonder-collab <furkanonder@gmail.com>
2026-07-08 19:01:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Şok Nokta - çevrimdışı önbellek (service worker)
-var CACHE = 'sok-nokta-v6';
+var CACHE = 'sok-nokta-v7';
 var ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', function(e) {
